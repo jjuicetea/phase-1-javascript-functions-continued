7d960e61c0271c4320b259c34ba7bbb25ff8d8a8
James Giusti <jamez318@gmail.com>
2021-09-07 15:13:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+	return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(task = "go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+
+//function wrapAdjective(msg = "You are"){
+  //  const innerFunction = function(rando = "special") {
+     //   return `${msg} ${rando}.`;
+  //  };
+   // return innerFunction("*");
+ //   }
+
+
+function wrapAdjective(sym = "*") {
+    return function b(adjective = "special"){
+        return "You are " + sym + adjective + sym + "!";}
+    }
+
